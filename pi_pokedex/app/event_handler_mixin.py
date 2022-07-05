0cb51a53f5251719fa9753f145cb0408296661c4
Andrew Seaman <andrewseaman35@gmail.com>
2022-07-05 15:18:58 -0700

    init eventhandlermixin once

--- pi_pokedex/app/event_handler_mixin.py
+++ pi_pokedex/app/event_handler_mixin.py
@@ -38,7 +38,10 @@ KEY_EVENT_MAP = {
 }
 
 
+
 class EventHandlerMixin:
+    has_initialized_gpio = False
+
     def __init__(self, *args, **kwargs):
         self.event_map = {}
 
@@ -47,17 +50,19 @@ class EventHandlerMixin:
 
         if config.IS_RUNNING_ON_RPI:
             self.init_gpio()
+            self.add_event_detection()
+
         self.bind("<KeyPress>", self.on_keyboard_press)
 
-    def init_gpio(self):
-        print("GPIO INIT")
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        self._callback = None
-        for pin in PIN_EVENT_MAP.keys():
-            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-        self.add_event_detection()
+    @classmethod
+    def init_gpio(cls):
+        if not cls.has_initialized_gpio:
+            print("GPIO INIT")
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BOARD)
+            for pin in PIN_EVENT_MAP.keys():
+                GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            cls.has_initialized_gpio = True
 
     def init_event_map(self):
         self.event_map = {}
