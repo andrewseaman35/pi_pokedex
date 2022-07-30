a1b56877e545e62f0f2a61d4e71da3b7fa373254
Andrew Seaman <andrewseaman35@gmail.com>
2022-07-29 21:20:24 -0700

    Add exit/shutdown item in settings

--- pi_pokedex/app/frames/settings.py
+++ pi_pokedex/app/frames/settings.py
@@ -1,12 +1,37 @@
+import os
+
+import config
+
 from frames.navigation import NavigationFrame, NavigationEntry
 from utils import get_ip_address
+
+
+ACTION_SHUTDOWN = "shutdown"
+ACTION_EXIT = "exit"
 
 
 class SettingsFrame(NavigationFrame):
     def __init__(self, master, on_back=None):
         ip_address = get_ip_address()
+        exit_entry = (
+            NavigationEntry(ACTION_SHUTDOWN, "Shutdown")
+            if config.IS_RUNNING_ON_RPI else
+            NavigationEntry(ACTION_EXIT, "Exit")
+        )
         items = [
             NavigationEntry("ip", f"IP Address: {ip_address}"),
             NavigationEntry("version", "Version: 0.0.1"),
+            exit_entry,
         ]
-        super().__init__(master=master, items=items, on_back=on_back)
+        super().__init__(
+            master=master,
+            items=items,
+            on_back=on_back,
+            on_item_select=self._on_item_select,
+        )
+
+    def _on_item_select(self, action):
+        if action == ACTION_SHUTDOWN:
+            os.system("sudo shutdown -h now")
+        elif action == ACTION_EXIT:
+            self.master.destroy()
