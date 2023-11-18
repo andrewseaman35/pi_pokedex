5e3771a42101b8836e5366480239fbec7e60d2ef
Andrew <andrewseaman35@gmail.com>
2023-11-18 15:47:39 -0800

    more led stuff

--- pi_pokedex/app/led.py
+++ pi_pokedex/app/led.py
@@ -1,3 +1,7 @@
+import datetime
+import time
+import threading
+
 import config
 
 if config.IS_RUNNING_ON_RPI:
@@ -49,7 +53,7 @@ class GPIOMock:
 
 
 
-GPIO = GPIOMock()
+#GPIO = GPIOMock()
 
 
 class _LEDManager:
@@ -65,9 +69,35 @@ class _LEDManager:
     _LED_PINS = _LED_TRIO + [
         _LED_BULB,
     ]
+    
+    _PATTERNS = {
+        'forward': [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+        ],
+        'backward': [
+            [0, 0, 1],
+            [0, 1, 0],
+            [1, 0, 0],
+        ],
+        'wave': [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+        ],
+        'alternate': [
+            [1, 0, 1],
+            [0, 1, 0],
+        ],
+    }
 
     def __init__(self):
         print("LED INIT")
+        self.__running = False
+        self.__thread = threading.Thread(target=self._run)
+        
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         for pin in self._LED_PINS:
@@ -79,7 +109,7 @@ class _LEDManager:
 
     def set_trio(self, values):
         if isinstance(values, list):
-            assert len(list) == len(self._LED_TRIO)
+            assert len(values) == len(self._LED_TRIO)
             GPIO.output(
                 self._LED_TRIO,
                 [GPIO.HIGH if v else GPIO.LOW for v in values],
@@ -89,7 +119,39 @@ class _LEDManager:
                 self._LED_TRIO,
                 [GPIO.HIGH if values else GPIO.LOW for _ in self._LED_TRIO]
             )
+            
+    def reset(self):
+        self.set_bulb(0)
+        self.set_trio(0)
+    
+    def start(self, pattern, duration):
+        if self.__running:
+            raise Exception("LEDs already running")
+        
+        self.__running = True
+        self.__thread = threading.Thread(target=lambda: self.run(pattern, duration))
+        self.__thread.start()
+        
+    def stop(self):
+        self.__running = False
+        self.join()
+        self.reset()
+    
+    def join(self):
+        if self.__thread.is_alive():
+            self.__thread.join()
 
+    def run(self, pattern, delay):
+        self.__running = True
+        assert delay >= 0
+        values = self._PATTERNS[pattern]
+        
+        while self.__running:
+            for p in values:
+                self.set_trio(p)
+                if not self.__running:
+                    break
+                time.sleep(delay)
 
 
 def LEDManager():
