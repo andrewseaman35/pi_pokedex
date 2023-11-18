2b367e8eaf93bb447318bf8fab92a846bb0a230e
Andrew <andrewseaman35@gmail.com>
2023-11-18 15:57:49 -0800

    remuse led mock

--- pi_pokedex/app/led.py
+++ pi_pokedex/app/led.py
@@ -2,58 +2,13 @@ import datetime
 import time
 import threading
 
-import config
-
-if config.IS_RUNNING_ON_RPI:
-    import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 # Channel numbers, not GPIO numbers
 LED_LEFT = 8
 LED_MID = 10
 LED_RIGHT = 12
 LED_BULB = 7
-
-class GPIOMock:
-    BOARD = "board"
-    OUT = "out"
-    LOW = "low"
-    HIGH = "high"
-
-    def __init__(self):
-        self._state = {}
-
-    def setwarnings(self, v):
-        pass
-
-    def setmode(self, v):
-        pass
-
-    def setup(self, pin, val):
-        print(f"setup: {pin} - {val}")
-
-    def _output(self, pin, val):
-        print(f"WRITE: {pin} - {val}")
-        self._state[pin] = val
-
-    def output(self, pin, val):
-        if isinstance(pin, list):
-            if isinstance(val, list):
-                for i, p in enumerate(pin):
-                    self._output(p, val[i])
-            else:
-                for p in pin:
-                    self._output(p, val)
-        else:
-            self._output(pin, val)
-
-    def input(self, pin):
-        val = self._state[pin]
-        print(f"READ: {pin} - {val}")
-        return val
-
-
-
-#GPIO = GPIOMock()
 
 
 class _LEDManager:
