90bb9f69e8a5ea5237182ef11d9e45f4b8b89c5b
Andrew Seaman <andrewseaman35@gmail.com>
2023-11-25 10:56:35 -0800

    add upload

--- pi_pokedex/app/api.py
+++ pi_pokedex/app/api.py
@@ -1,15 +1,7 @@
 import time
-start = time.time()
-print(f"Start: {start}")
-
 import json
-import os
 import boto3
-import traceback
 now_1 = time.time()
-print(f"Imports done: {now_1 - start}")
-
-
 
 
 class _IdentifierApi:
@@ -19,21 +11,33 @@ class _IdentifierApi:
         self.aws_session = boto3.session.Session(profile_name="pokedex")
         self.ssm_client = self.aws_session.client("ssm", region_name="us-east-1")
         self.lambda_client = self.aws_session.client("lambda", region_name="us-east-1")
-        self.config = {}
+        self.s3_client = self.aws_session.client("s3", region_name="us-east-1")
 
-        self._initialize_config()
+        self._get_ssm_config()
 
-    def _initialize_config(self):
+    def _get_ssm_config(self):
         self.lambda_function_name = self._get_from_ssm("/pokedex/live/identifier_lambda_function_name")
+        self.s3_bucket_name = self._get_from_ssm("/pokedex/live/s3_bucket_name")
+        self.s3_key_prefix = self._get_from_ssm("/pokedex/live/s3_key_prefix")
 
     def _get_from_ssm(self, key):
         response = self.ssm_client.get_parameter(Name=key)
         return response["Parameter"]["Value"]
 
-    def upload(filepath):
-        pass
+    def upload(self, filepath, source, group):
+        start = time.time()
+        print(f"Start upload: {start}")
+        key = f"{self.s3_key_prefix}{source}/{group}/original.jpg"
+        with open(filepath, 'rb') as f:
+            self.s3_client.put_object(
+                Body=f,
+                Bucket=self.s3_bucket_name,
+                Key=key,
+            )
+        print(f"End upload: {time.time() - start}")
+        return key
 
-    def identify(self, filepath):
+    def identify(self, source, group):
         start = time.time()
         print(f"Start identify: {start}")
 
@@ -42,8 +46,8 @@ class _IdentifierApi:
             InvocationType='RequestResponse',
             LogType='None',
             Payload=json.dumps({
-                'source': 'test',
-                'group': 'test1'
+                'source': source,
+                'group': group,
             }),
         )
 
@@ -57,3 +61,7 @@ def IdentifierApi():
     if _IdentifierApi._instance is None:
         _IdentifierApi._instance = _IdentifierApi()
     return _IdentifierApi._instance
+
+
+filepath = '/Users/andrewseaman/repos/pi_pokedex/pi_pokedex/assets/img/pokemon/1.png'
+IdentifierApi().upload(filepath, 'test', 'test2')
