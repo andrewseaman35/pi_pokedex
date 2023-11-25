652aceaefff9b6314cbd282d6877a25e6b9ba545
Andrew Seaman <andrewseaman35@gmail.com>
2023-11-25 10:38:42 -0800

    move api to class

--- pi_pokedex/app/api.py
+++ pi_pokedex/app/api.py
@@ -9,45 +9,51 @@ import traceback
 now_1 = time.time()
 print(f"Imports done: {now_1 - start}")
 
-def identify(filepath):
-    try:
-        aws_session = boto3.session.Session(profile_name="pokedex")
 
-        ssm_client = aws_session.client("ssm", region_name="us-east-1")
-        lambda_client = aws_session.client("lambda", region_name="us-east-1")
-    except Exception as e:
-        print(traceback.format_exc())
-        return "fuck"
 
-    def get_from_ssm(key):
-        response = ssm_client.get_parameter(Name=key)
+
+class _IdentifierApi:
+    _instance = None
+
+    def __init__(self):
+        self.aws_session = boto3.session.Session(profile_name="pokedex")
+        self.ssm_client = self.aws_session.client("ssm", region_name="us-east-1")
+        self.lambda_client = self.aws_session.client("lambda", region_name="us-east-1")
+        self.config = {}
+
+        self._initialize_config()
+
+    def _initialize_config(self):
+        self.lambda_function_name = self._get_from_ssm("/pokedex/live/identifier_lambda_function_name")
+
+    def _get_from_ssm(self, key):
+        response = self.ssm_client.get_parameter(Name=key)
         return response["Parameter"]["Value"]
 
-    lambda_function_name = get_from_ssm("/pokedex/live/identifier_lambda_function_name")
-    now_ssm = time.time()
-    print(f"Got SSM: {now_ssm - now_2}")
+    def upload(filepath):
+        pass
 
-    response = lambda_client.invoke(
-        FunctionName=lambda_function_name,
-        InvocationType='RequestResponse',
-        LogType='None',
-        Payload=json.dumps({
-            'source': 'test',
-            'group': 'test1'
-        }),
-    )
+    def identify(self, filepath):
+        start = time.time()
+        print(f"Start identify: {start}")
 
-    # print(json.loads(response['Payload']))
-    print(response)
-    json_response = response['Payload'].read()
-    print(json_response)
-    return 1
+        response = self.lambda_client.invoke(
+            FunctionName=self.lambda_function_name,
+            InvocationType='RequestResponse',
+            LogType='None',
+            Payload=json.dumps({
+                'source': 'test',
+                'group': 'test1'
+            }),
+        )
 
-now_2 = time.time()
-print(f"Define done: {now_2 - now_1}")
-response = identify("asdf")
-print(response)
-now_3 = time.time()
-print(f"Identify done: {now_3 - now_2}")
+        print(response)
+        json_response = response['Payload'].read()
+        print(json_response)
+        print(f"Done: {time.time() - start}")
+        return 1
 
-print(f"TOTAL: {time.time() - start}")
+def IdentifierApi():
+    if _IdentifierApi._instance is None:
+        _IdentifierApi._instance = _IdentifierApi()
+    return _IdentifierApi._instance
